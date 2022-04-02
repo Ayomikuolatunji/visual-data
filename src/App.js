34cdc56f8d7f6347c7b5195252a27c23f4cf0e45
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-04-02 18:34:09 +0100

    d3 installation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+
+
+const width=960;
+const height=960;
+
+const centerX=width/2;
+const centerY=height/2;
+const strokwidth=5;
+const eyeOffsetX=90;
+const eyeOffsetY=100
+const eyeRadius=40;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Apsp">
+         <svg width={width} height={height}>
+             <circle 
+             r="200"
+             cx={centerX}
+             cy={centerY}
+             fill='yellow'
+             stroke='black'
+             strokeWidth={strokwidth}
+             >
+             </circle>
+             <circle
+               cx={centerX - eyeOffsetX}
+               cy={centerY - eyeOffsetY}
+               r={eyeRadius}
+             >
+             </circle>
+             <circle
+               cx={centerX + eyeOffsetX}
+               cy={centerY - eyeOffsetY}
+               r={eyeRadius}
+             >
+             </circle>
+
+         </svg>
     </div>
   );
 }
